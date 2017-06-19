46a58ae5fc56a1ef040803d0f4749ff4c7e98571
crisroco <crc1992_3@hotmail.com>
2017-06-18 23:52:59 -0500

    select ajax complete2

--- scormList.php
+++ scormList.php
@@ -8,18 +8,17 @@ require_once($CFG->libdir.'/completionlib.php');
 require_once('locallib.php');
 
 
+//Get parameter
 $courseid = $_POST['courseid'];
 $groupid = $_POST['groupid'];
 
 $list_scorms = get_mod_availability($courseid);
 $allscorms = get_scorms($courseid);
 
-$temparrayu = array();
 $options='';
 
 foreach ($list_scorms[$groupid] as $key => $value) {
-   $temparrayu[$key] = $allscorms[$value];
-   $options .= '<option value="'.$key.'">'.$allscorms[$value].'</option>';
+   $options .= '<option value="'.$value.'">'.$allscorms[$value].'</option>';
 }
 
 
