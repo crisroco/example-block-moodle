4a13248392ade4867e22ae00e04602dce40f2cfe
Pablo Cordova <pcordova@atypax.com>
2017-06-22 09:34:04 -0500

    button generar reporte de scrom with size 10

--- block_scorm_report.php
+++ block_scorm_report.php
@@ -1,37 +1,39 @@
 <?php
 
 class block_scorm_report extends block_base {
-	function init() {
-    	$this->title = get_string('pluginname', 'block_scorm_report');
-	}
+    function init() {
+        $this->title = get_string('pluginname', 'block_scorm_report');
+    }
 
-	function has_config() {
-		return true;
-	}
+    function has_config() {
+        return true;
+    }
 
-	function get_content() {
-		global $OUTPUT, $USER , $DB , $COURSE;
-      
-	    $courseid = $this->page->course->id;
-	    $userid = $USER->id;
+    function get_content() {
+        global $OUTPUT, $USER , $DB , $COURSE, $PAGE;
 
-	    if ($this->content !== null) {
-	      return $this->content;
-	    }
+        $PAGE->requires->css('/blocks/scorm_report/assets/css/styles.css');
 
-	    if (empty($this->instance)) {
-	      $this->content = '';
-	      return $this->content;
-	    }
+        $courseid = $this->page->course->id;
+        $userid = $USER->id;
 
-	    $this->content = new stdClass();
-	    $this->content->text = ''; //translate this
+        if ($this->content !== null) {
+          return $this->content;
+        }
 
-	    $url = new moodle_url('/blocks/scorm_report/view.php', array('courseid' =>$courseid, 'userid' => $userid));
+        if (empty($this->instance)) {
+          $this->content = '';
+          return $this->content;
+        }
 
-	    $text = 'Generar Reporte de Scorm'; //Translate this
-	    //$this->content->text = '<div>Este es un curso hijo</div>';
-	    $this->content->text .= html_writer::link($url,$text,array('class'=>'btn btn-danger' , 'target' => '_blank'));
-	}
+        $this->content = new stdClass();
+        $this->content->text = ''; //translate this
+
+        $url = new moodle_url('/blocks/scorm_report/view.php', array('courseid' =>$courseid, 'userid' => $userid));
+
+        $text = 'Generar Reporte de Scorm'; //Translate this
+        //$this->content->text = '<div>Este es un curso hijo</div>';
+        $this->content->text .= html_writer::link($url,$text,array('class'=>'btn btn-danger btn-reporte' , 'target' => '_blank'));
+    }
 
 }
